92dfdbc62722cadf8ad772f98c9f89a2b90e2e4f
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2023-10-17 03:41:17 +0800

    Add files via upload

---
+++ SalesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddSomeShopWeb.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class SalesController : Controller
+	{
+		public IActionResult PriceCheck()
+		{
+			return View();
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Returnandrefund()
+		{
+			return View();
+		}
+
+		public IActionResult Payment()
+		{
+			return View();
+		}
+		public IActionResult Addproduct()
+		{
+			return View();
+		}
+	}
+}
